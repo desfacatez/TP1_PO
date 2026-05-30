b20788547082e38e693dd37619355aa7184b6251
Kalyan <rosalino.serra@aluno.ufop.edu.br>
2026-05-30 17:37:40 -0300

    Refactor Cliente class and add private access specifiers

--- Cliente.h
+++ Cliente.h
@@ -1,10 +1,11 @@
-#ifndef     CLIENTE_H
-#define     CLIENTE_H
+#ifndef CLIENTE_H
+#define CLIENTE_H
 #include "Pessoa.h"
 #include "Transacao.h"
 #include <vector>
 
 class Cliente : public Pessoa {
+private:
     string dataNascimento;              
     double remuneracao;                 
     string tipoConta;                   
@@ -20,24 +21,23 @@ public:
             const string& tipoConta, double taxaDeRendimento,
             double saldo, const string& login, const string& senha);
 
-    string getDataNascimento()            const { return dataNascimento; }
-    double getRemuneracao()               const { return remuneracao; }
-    string getTipoConta()                 const { return tipoConta; }
-    double getRendimento()                const { return taxaDeRendimento; }
-    double getSaldo()                     const { return saldo; }
-    const vector<Transacao>& getExtrato() const { return transacoes; }
+    string getDataNascimento() const;
+    double getRemuneracao() const;
+    string getTipoConta() const;
+    double getRendimento() const;
+    double getSaldo() const;
+    const vector<Transacao>& getExtrato() const;
    
 
-    void setDataNascimento(const string& d) { dataNascimento = d; }
-    void setRemuneracao(double r)           { remuneracao = r; }
-    void setTipoConta(const string& t)      { tipoConta = t; }
-    void setRendimento(double t)            { taxaDeRendimento = t; }
-    void setSaldo(double s)                 { saldo = s; }
-
-    void SetTransacao(const Transacao& t) { transacoes.push_back(t); }
-
+    void setDataNascimento(const string& d);
+    void setRemuneracao(double r);
+    void setTipoConta(const string& t);
+    void setRendimento(double t);
+    void setSaldo(double s);
+    void SetTransacao(const Transacao& t);
+    
     void aplicarRendimento();
-
+    void criarCartao();
     void exibirDados() const override; 
     
 };
